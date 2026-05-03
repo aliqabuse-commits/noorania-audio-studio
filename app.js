20c292f755f9db9f16b548f2ed53b21e44ac1617
Ali Ahmad <aliqabuse@gmail.com>
2026-05-03 16:58:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
+const units = [
+  { text: "بَ", file: "ba_fatha.wav" },
+  { text: "بِ", file: "ba_kasra.wav" },
+  { text: "بُ", file: "ba_damma.wav" },
+
+  { text: "أَبْ", file: "ba_sukoon_after_fatha.wav" },
+  { text: "إِبْ", file: "ba_sukoon_after_kasra.wav" },
+  { text: "أُبْ", file: "ba_sukoon_after_damma.wav" },
+
+  { text: "أَمْ", file: "meem_sukoon_after_fatha.wav" },
+  { text: "أَنْ", file: "noon_sukoon_after_fatha.wav" },
+  { text: "أُلْ", file: "lam_sukoon_after_damma.wav" }
+];
+
+let index = 0;
+
 let mediaRecorder;
 let audioChunks = [];
 let audioBlob;
+
+function updateUI() {
+  document.getElementById("unit").innerText = units[index].text;
+}
+
+updateUI();
 
 async function startRecording() {
   const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -25,13 +47,22 @@ function stopRecording() {
 }
 
 function play() {
+  if (!audioBlob) return;
   const audio = new Audio(URL.createObjectURL(audioBlob));
   audio.play();
 }
 
 function download() {
+  if (!audioBlob) return;
+
   const a = document.createElement("a");
   a.href = URL.createObjectURL(audioBlob);
-  a.download = "record.wav";
+  a.download = units[index].file;
   a.click();
+}
+
+function nextUnit() {
+  index++;
+  if (index >= units.length) index = 0;
+  updateUI();
 }
