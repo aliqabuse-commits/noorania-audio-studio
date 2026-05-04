a0e183c9111186eaddd7f8f2c82ff30d877fa4fb
Ali Ahmad <aliqabuse@gmail.com>
2026-05-04 17:32:32 +0300

    Update recording-manifest.js

--- recording-manifest.js
+++ recording-manifest.js
@@ -1,1 +1,13 @@
+window.allUnits = {
+  "الحروف المتحركة": [
+    { text: "بَ", file: "ba_fatha.wav" },
+    { text: "بِ", file: "ba_kasra.wav" },
+    { text: "بُ", file: "ba_damma.wav" }
+  ],
 
+  "الحروف الساكنة": [
+    { text: "أَبْ", file: "ba_sukoon_after_fatha.wav" },
+    { text: "إِبْ", file: "ba_sukoon_after_kasra.wav" },
+    { text: "أُبْ", file: "ba_sukoon_after_damma.wav" }
+  ]
+};
